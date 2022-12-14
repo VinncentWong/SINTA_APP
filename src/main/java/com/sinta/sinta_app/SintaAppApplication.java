b43bbd51e50fa5ddf259c1dc6843d2e2b54dfb38
VinncentWong <vinncentwong2@gmail.com>
2022-12-14 16:25:49 +0700

    init

---
+++ src/main/java/com/sinta/sinta_app/SintaAppApplication.java
@@ -0,0 +1,13 @@
+package com.sinta.sinta_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinta/sinta_app/SintaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinta.sinta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SintaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

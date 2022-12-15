b7120475319ec7b99807eb287fba1a6d9b5dd154
VinncentWong <vinncentwong2@gmail.com>
2022-12-15 16:01:26 +0700

    add cors customizer

--- src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
+++ src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.sinta.sinta_app.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,6 +16,15 @@ public class SecurityConfiguration {
         return httpSecurity.
                 httpBasic().disable().
                 csrf().disable().
+                cors((customizer) -> {
+                    customizer.configurationSource((request) -> {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedHeaders(List.of("*"));
+                        corsConfiguration.setAllowedMethods(List.of("*"));
+                        corsConfiguration.setAllowedOrigins(List.of("*"));
+                        return corsConfiguration;
+                    });
+                }).
                 authorizeRequests().anyRequest().permitAll().
                 and()
                 .build();

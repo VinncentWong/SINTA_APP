da67e05aebb697a4f9dad7a1acebc2a30ff4064c
VinncentWong <vinncentwong2@gmail.com>
2022-12-15 15:04:43 +0700

    add AgentNotFoundException

---
+++ src/main/java/com/sinta/sinta_app/exception/AgentNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sinta.sinta_app.exception;
+
+public class AgentNotFoundException extends Exception{
+
+    public AgentNotFoundException() {
+    }
+
+    public AgentNotFoundException(String arg0) {
+        super(arg0);
+    }
+    
+}

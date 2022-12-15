6dd126f468d512b1e62427205fd4b300d5454afe
Vinncent Alexander Wong <88434717+VinncentWong@users.noreply.github.com>
2022-12-15 15:06:40 +0700

    Delete src/main/java/com/sinta/sinta_app/app/repositories directory

--- src/main/java/com/sinta/sinta_app/app/repositories/AgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sinta.sinta_app.app.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.sinta.sinta_app.entity.agent.Agent;
-
-@Repository
-public interface AgentRepository extends CrudRepository<Agent, Long>{}

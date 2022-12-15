9bfd2118cb10428a5c6527c52cb3b8e677632c39
VinncentWong <vinncentwong2@gmail.com>
2022-12-15 15:05:45 +0700

    add Trip repositories

---
+++ src/main/java/com/sinta/sinta_app/app/trip/TripRepository.java
@@ -0,0 +1,9 @@
+package com.sinta.sinta_app.app.trip;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sinta.sinta_app.entity.trip.Trip;
+
+@Repository
+public interface TripRepository extends CrudRepository<Trip, Long>{}

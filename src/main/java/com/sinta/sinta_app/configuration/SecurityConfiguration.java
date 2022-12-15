713181ba41c848d352cb222df9f88cd98399596c
VinncentWong <vinncentwong2@gmail.com>
2022-12-15 15:34:19 +0700

    add configuration

--- src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
+++ src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
@@ -1,13 +1,20 @@
 package com.sinta.sinta_app.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfiguration {
     
     @Bean
     public SecurityFilterChain securityConfig(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.httpBasic().disable().csrf().disable().build();
+        return httpSecurity.
+                httpBasic().disable().
+                csrf().disable().
+                authorizeRequests().anyRequest().permitAll().
+                and()
+                .build();
     }
 }

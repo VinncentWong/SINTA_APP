f672a1c8bfcbc47de052520eeae507edaba37377
VinncentWong <vinncentwong2@gmail.com>
2022-12-16 17:15:13 +0700

    Add TripNotFoundException

---
+++ src/main/java/com/sinta/sinta_app/exception/TripNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sinta.sinta_app.exception;
+
+public class TripNotFoundException extends Exception{
+
+    public TripNotFoundException() {
+    }
+
+    public TripNotFoundException(String arg0) {
+        super(arg0);
+    }
+    
+}

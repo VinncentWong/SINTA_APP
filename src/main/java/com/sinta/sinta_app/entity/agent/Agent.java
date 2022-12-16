690acbbc6ebf5a3931c8ec605f1b5536fa362202
VinncentWong <vinncentwong2@gmail.com>
2022-12-16 17:14:13 +0700

    remove some annotation due issue to performance

--- src/main/java/com/sinta/sinta_app/entity/agent/Agent.java
+++ src/main/java/com/sinta/sinta_app/entity/agent/Agent.java
@@ -1,17 +1,23 @@
 package com.sinta.sinta_app.entity.agent;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sinta.sinta_app.entity.Role;
+import com.sinta.sinta_app.entity.trip.Trip;
 
 import lombok.Data;
 
@@ -43,7 +49,7 @@ public class Agent {
 
     private String akunInstagram;
 
-    @Lob
+    @JsonIgnore
     private byte[] suratIzinUsaha;
 
     private String linkGmaps;
@@ -63,6 +69,9 @@ public class Agent {
     private LocalDate updatedAt;
 
     private LocalDate deletedAt;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "agent")
+    private List<Trip> trip;
 
     private final Role role = Role.AGENT;
 }

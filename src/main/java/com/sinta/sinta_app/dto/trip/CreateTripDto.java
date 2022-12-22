cad7b49f54d96b0891fd981388c9253e1a649230
VinncentWong <vinncentwong2@gmail.com>
2022-12-22 17:06:39 +0700

    Add new property

--- src/main/java/com/sinta/sinta_app/dto/trip/CreateTripDto.java
+++ src/main/java/com/sinta/sinta_app/dto/trip/CreateTripDto.java
@@ -2,6 +2,8 @@ package com.sinta.sinta_app.dto.trip;
 
 import java.util.List;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -9,6 +11,7 @@ import com.sinta.sinta_app.dto.trip.deskripsi.DeskripsiDto;
 import com.sinta.sinta_app.dto.trip.fasilitas.FasilitasTermasukDto;
 import com.sinta.sinta_app.dto.trip.fasilitas.FasilitasTidakTermasukDto;
 import com.sinta.sinta_app.dto.trip.harga.HargaDto;
+import com.sinta.sinta_app.entity.trip.KategoriTrip;
 
 public record CreateTripDto(
     @NotNull
@@ -27,5 +30,19 @@ public record CreateTripDto(
     @NotNull
     List<FasilitasTidakTermasukDto> fasilitasTidakTermasuk,
     @NotNull
-    List<HargaDto> harga
+    List<HargaDto> harga,
+    @NotNull
+    @NotBlank
+    String kota,
+    @NotNull
+    @NotBlank
+    String provinsi,
+    @NotNull
+    @NotBlank
+    String lamaTrip,
+    @NotNull
+    Boolean needRequirement,
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    KategoriTrip kategoriTrip
 ) {}

0cedd5d05ac8ec7185528e2d6f37e7327ff2c132
VinncentWong <vinncentwong2@gmail.com>
2022-12-23 23:32:09 +0700

    Add new depedency

--- src/main/java/com/sinta/sinta_app/SintaAppApplication.java
+++ src/main/java/com/sinta/sinta_app/SintaAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SintaAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SintaAppApplication.class, args);
 	}
-
 }

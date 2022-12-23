84058a392834f21dbe85c7e8d75298d150d385fb
VinncentWong <vinncentwong2@gmail.com>
2022-12-23 23:22:45 +0700

    add Extractable and StatusVerified

---
+++ src/main/java/com/sinta/sinta_app/entity/Extractable.java
@@ -0,0 +1,7 @@
+package com.sinta.sinta_app.entity;
+
+public interface Extractable {
+    String getUsername();
+    Long getId();
+    Role getRole();
+}
---
+++ src/main/java/com/sinta/sinta_app/entity/StatusVerified.java
@@ -0,0 +1,5 @@
+package com.sinta.sinta_app.entity;
+
+public enum StatusVerified {
+    BELUM_TERVERIFIKASI, MENUNGGU, TERVERIFIKASI, TIDAK_TERVERIFIKASI
+}

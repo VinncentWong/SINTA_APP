c2b3039b491e5dd1e49c597ab52d4f158ec298f6
VinncentWong <vinncentwong2@gmail.com>
2022-12-23 23:22:17 +0700

    adjust security config

--- src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
+++ src/main/java/com/sinta/sinta_app/configuration/SecurityConfiguration.java
@@ -2,18 +2,30 @@ package com.sinta.sinta_app.configuration;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+
+import com.sinta.sinta_app.security.filter.JwtFilter;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final JwtFilter filter;
+
+    @Autowired
+    public SecurityConfiguration(JwtFilter filter){
+        this.filter = filter;
+    }
     
     @Bean
     public SecurityFilterChain securityConfig(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.
+                addFilterAfter(filter, UsernamePasswordAuthenticationFilter.class).
                 httpBasic().disable().
                 csrf().disable().
                 cors((customizer) -> {
@@ -25,7 +37,9 @@ public class SecurityConfiguration {
                         return corsConfiguration;
                     });
                 }).
-                authorizeRequests().anyRequest().permitAll().
+                authorizeRequests()
+                .mvcMatchers("/trip/create/**").authenticated()
+                .anyRequest().permitAll().
                 and()
                 .build();
     }
